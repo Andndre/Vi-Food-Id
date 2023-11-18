0cfd9c03733d2a49759175173887f111fac2a2f1
andndre <agungandre687@gmail.com>
2023-11-18 20:57:25 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // mode: 'jit',
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFB649"
+      },
+    },
+  },
+  plugins: [],
+  important: true
+};

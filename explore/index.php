9d74941153c8cf3649e876fbb0c556af65fdcc86
andndre <agungandre687@gmail.com>
2023-11-19 10:53:16 +0800

    Integrasi database untuk halaman Explore

--- explore/index.php
+++ explore/index.php
@@ -1,29 +1,33 @@
+<?php
+	$host = 'localhost';
+	$username = 'root';
+	$password = '999572163';
+	$database = 'vi-food';
+
+	$koneksi = new mysqli($host, $username, $password, $database);
+
+	// Periksa koneksi
+	if ($koneksi->connect_error) {
+			die("Koneksi gagal: " . $koneksi->connect_error);
+	}
+
+	$query = "SELECT * FROM menu";
+	$semua_menu = $koneksi->query($query);
+?>
+
 <!DOCTYPE html>
 <html lang="id" class="scroll-smooth">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VI Food</title>
-    <link rel="stylesheet" href="../css/dist/output.css">
-		<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js' integrity='sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==' crossorigin='anonymous'></script>
-		<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.js"></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>VI Food</title>
+	<link rel="stylesheet" href="../css/dist/output.css">
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js' integrity='sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==' crossorigin='anonymous'></script>
+	<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.js"></script>
 </head>
 <body class="bg-gray-200 pb-6">
 	<!-- Navbar -->
-	<nav class="bg-white shadow-sm">
-		<div class="flex items-center h-16 justify-between w-full gap-6 container mx-auto px-4">
-			<a href="/"><img src="../public/logo.svg" alt="logo" class="h-5"></a>
-			<div class="flex gap-4 items-center">
-				<div class="flex gap-3 items-center">
-					<a class="flex gap-2 items-center px-6 py-3 bg-primary/10 hover:bg-primary/25 transition-all duration-300 rounded-md" href="../pembelian"><i data-feather="bar-chart-2"></i> Pembelian</a>
-					<a class="flex gap-2 items-center px-6 py-3 bg-primary/10 hover:bg-primary/25 transition-all duration-300 rounded-md" href="../keranjang/"><i data-feather="shopping-cart"></i> Keranjang</a>
-				</div>
-				<a href="../profil/">
-					<div class="w-10 h-10 bg-gray-400 rounded-full"></div>
-				</a>
-			</div>
-		</div>
-	</nav>
+	<?php require "../components/navbar.php" ?>
 	<!-- Header w/Search -->
 	<div class="container mx-auto">
 		<div class="mx-4 mt-4 bg-cover rounded-2xl px-6 py-24 lg:py-36 bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsouIA8z5MLF0d2Z7NnunT8Sp7QvVaqbFCZA&usqp=CAU')] text-white text-center space-y-4">
@@ -89,162 +93,34 @@
 				<h2 class="font-bold text-xl">Rekomendasi</h2>
 				<!-- Grup Menu Rekomendasi -->
 				<div class="grid grid-cols-12 pt-4 gap-4">
-					<!-- Card Start -->
-					<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
-						<img class="w-full aspect-video object-cover" src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/08/01045246/ini-resep-mie-goreng-dengan-bahan-sederhana-yang-menggugah-selera-halodoc.jpg.webp" alt="">
-						<div class="p-4 space-y-2 text-sm">
-							<h3 class="font-bold text-xl">Mie Goreng Sehat<h3>
-							<div class="space-y-2">
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map-pin" class="h-5 mr-2"></i>
-									Jalan Wijaya Kusuma Gg 3 Singaraja
+					<?php while ($row = $semua_menu->fetch_assoc()): ?>
+						<!-- Card Start -->
+						<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
+							<img class="w-full aspect-video object-cover" src="<?= $row['gambar']?>" alt="">
+							<div class="p-4 space-y-2 text-sm">
+								<h3 class="font-bold text-xl"><?= $row['nama']?><h3>
+								<div class="space-y-2">
+									<div class="flex items-center text-gray-500 ">
+										<i data-feather="map-pin" class="h-5 mr-2"></i>
+										<?= $row['alamat']?>
+									</div>
+									<div class="flex items-center text-gray-500 ">
+										<i data-feather="map" class="h-5 mr-2"></i>
+										<?= $row['warung']?>
+									</div>
+									<div class="flex items-center text-gray-500">
+										<i data-feather="clock" class="h-5 mr-2"></i>
+										Buka <?= date("H:i", strtotime($row['buka']))?> - <?= date("H:i", strtotime($row['tutup']))?> WITA</div>
 								</div>
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map" class="h-5 mr-2"></i>
-									Warung Pak Wayan
+	
+								<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
+									<span class="text-lg text-gray-400 font-semibold"><?= 'Rp.' . number_format($row['harga'], 0, ',', '.') ?></span>
+									<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
 								</div>
-								<div class="flex items-center text-gray-500">
-									<i data-feather="clock" class="h-5 mr-2"></i>
-									Buka 08:00 - 18:00 WITA</div>
-							</div>
-
-							<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
-								<span class="text-lg text-gray-400 font-semibold">Rp. 12.000,00</span>
-								<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
-							</div>
-						</div>
-					</div>
-					<!-- Card End -->
-					<!-- Card Start -->
-					<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
-						<img class="w-full aspect-video object-cover" src="https://cdn.idntimes.com/content-images/post/20220414/258537864-4512524435507508-6572717919153111441-n-2dbb2782cce8e1857deda55946bcaa25_600x400.jpg" alt="">
-						<div class="p-4 space-y-2 text-sm">
-							<h3 class="font-bold text-xl">Babi Guling<h3>
-							<div class="space-y-2">
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map-pin" class="h-5 mr-2"></i>
-									Jalan Wijaya Kusuma Gg 3 Singaraja
-								</div>
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map" class="h-5 mr-2"></i>
-									Warung Pak Wayan
-								</div>
-								<div class="flex items-center text-gray-500">
-									<i data-feather="clock" class="h-5 mr-2"></i>
-									Buka 08:00 - 18:00 WITA</div>
-							</div>
-
-							<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
-								<span class="text-lg text-gray-400 font-semibold">Rp. 12.000,00</span>
-								<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
 							</div>
 						</div>
-					</div>
-					<!-- Card End -->
-					<!-- Card Start -->
-					<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
-						<img class="w-full aspect-video object-cover" src="https://www.astronauts.id/blog/wp-content/uploads/2023/01/Ini-Resep-Bakso-Urat-yang-Empuk-dan-Lembut-1200x900.jpg" alt="">
-						<div class="p-4 space-y-2 text-sm">
-							<h3 class="font-bold text-xl">Bakso Urat<h3>
-							<div class="space-y-2">
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map-pin" class="h-5 mr-2"></i>
-									Jalan Wijaya Kusuma Gg 3 Singaraja
-								</div>
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map" class="h-5 mr-2"></i>
-									Warung Pak Wayan
-								</div>
-								<div class="flex items-center text-gray-500">
-									<i data-feather="clock" class="h-5 mr-2"></i>
-									Buka 08:00 - 18:00 WITA</div>
-							</div>
-
-							<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
-								<span class="text-lg text-gray-400 font-semibold">Rp. 12.000,00</span>
-								<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
-							</div>
-						</div>
-					</div>
-					<!-- Card End -->
-					<!-- Card Start -->
-					<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
-						<img class="w-full aspect-video object-cover" src="https://cdn0-production-images-kly.akamaized.net/P20zwAGcL1t5dpmJCUGjlZjqU70=/1x68:1000x631/469x260/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4525355/original/040795900_1691112723-shutterstock_2190434085.jpg" alt="">
-						<div class="p-4 space-y-2 text-sm">
-							<h3 class="font-bold text-xl">Batagor Tahu<h3>
-							<div class="space-y-2">
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map-pin" class="h-5 mr-2"></i>
-									Jalan Wijaya Kusuma Gg 3 Singaraja
-								</div>
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map" class="h-5 mr-2"></i>
-									Warung Pak Wayan
-								</div>
-								<div class="flex items-center text-gray-500">
-									<i data-feather="clock" class="h-5 mr-2"></i>
-									Buka 08:00 - 18:00 WITA</div>
-							</div>
-
-							<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
-								<span class="text-lg text-gray-400 font-semibold">Rp. 12.000,00</span>
-								<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
-							</div>
-						</div>
-					</div>
-					<!-- Card End -->
-					<!-- Card Start -->
-					<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
-						<img class="w-full aspect-video object-cover" src="https://images.tokopedia.net/img/cache/700/VqbcmM/2021/3/4/1293dd51-c3cf-465d-97d7-62516be91e5f.jpg" alt="">
-						<div class="p-4 space-y-2 text-sm">
-							<h3 class="font-bold text-xl">Siobak Jaen<h3>
-							<div class="space-y-2">
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map-pin" class="h-5 mr-2"></i>
-									Jalan Wijaya Kusuma Gg 3 Singaraja
-								</div>
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map" class="h-5 mr-2"></i>
-									Warung Pak Wayan
-								</div>
-								<div class="flex items-center text-gray-500">
-									<i data-feather="clock" class="h-5 mr-2"></i>
-									Buka 08:00 - 18:00 WITA</div>
-							</div>
-
-							<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
-								<span class="text-lg text-gray-400 font-semibold">Rp. 12.000,00</span>
-								<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
-							</div>
-						</div>
-					</div>
-					<!-- Card End -->
-					<!-- Card Start -->
-					<div class="overflow-hidden col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-2xl shadow-sm">
-						<img class="w-full aspect-video" src="https://resepkoki.id/wp-content/uploads/2016/11/Resep-Fuyunghai.jpg" alt="">
-						<div class="p-4 space-y-2 text-sm">
-							<h3 class="font-bold text-xl">Fuyunghai<h3>
-							<div class="space-y-2">
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map-pin" class="h-5 mr-2"></i>
-									Jalan Wijaya Kusuma Gg 3 Singaraja
-								</div>
-								<div class="flex items-center text-gray-500 ">
-									<i data-feather="map" class="h-5 mr-2"></i>
-									Warung Pak Wayan
-								</div>
-								<div class="flex items-center text-gray-500">
-									<i data-feather="clock" class="h-5 mr-2"></i>
-									Buka 08:00 - 18:00 WITA</div>
-							</div>
-
-							<div class="flex justify-between items-center flex-wrap gap-3 pt-4">
-								<span class="text-lg text-gray-400 font-semibold">Rp. 12.000,00</span>
-								<a class="px-6 py-3 ml-auto bg-primary/20 hover:bg-primary transition-all duration-300 font-semibold rounded-lg" href="/tempat/">Lihat</a>
-							</div>
-						</div>
-					</div>
-					<!-- Card End -->
+						<!-- Card End -->
+					<?php endwhile; ?>
 				</div>
 			</div>
 		</div>
